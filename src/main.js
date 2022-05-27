e6f15039cf610665946c237b3900e43af76f82a4
tbru <tbru@itu.dk>
2022-05-27 20:39:29 +0200

    add maps API to project

--- src/main.js
+++ src/main.js
@@ -46,3 +46,52 @@ function signOutButton(){
     sb.addEventListener("click", e => {signOut(auth)});
     sb.removeAttribute('hidden');
 }
+
+function initMap(){
+   // The center on init
+  const copenhagen = { lat: 55.676098, lng: 12.568337 };
+
+  //map init options
+  const options = {
+    zoom: 11,
+    center: copenhagen,
+    streetViewControl: false,
+    mapTypeControl: false,
+  }
+   // The map, centered at Copenhagen
+  const mapCanvas = new google.maps.Map(document.getElementById("map"), options);
+
+  removeDefaultMapPins(mapCanvas);
+}
+window.initMap = initMap;
+
+function removeDefaultMapPins(map){
+  let removePOI = [
+    {
+      "featureType": "poi",
+      "elementType": "labels",
+      "stylers": [
+        { "visibility": "off" }
+      ]
+    },
+    {
+      "featureType": "poi.business",
+      "elementType": "all",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "transit",
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    }
+  ]
+  map.setOptions({styles: removePOI})
+}
